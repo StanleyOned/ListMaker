a78f6f6c0ae47c185173181b4133a65e404af873
Stanle De La Cruz <stanle@prolificinteractive.com>
2018-11-20 14:53:53 -0500

    Added Fragment for ListActivity

--- app/src/main/java/com/stanledelacruz/listmaker/ListActivity.kt
+++ app/src/main/java/com/stanledelacruz/listmaker/ListActivity.kt
@@ -10,18 +10,19 @@ import android.text.InputType
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.FrameLayout
 
 import kotlinx.android.synthetic.main.activity_list.*
 
-class ListActivity : AppCompatActivity(), ListSelectionRecyclerViewAdapter.ListSelectionRecyclerViewClickListener {
+class ListActivity : AppCompatActivity(), ListSelectionFragment.OnListItemFragmentInteractionListener {
 
     companion object {
         val INTENT_LIST_KEY = "list"
         val LIST_DETAIL_REQUEST_CODE = 123
     }
 
-    lateinit var listsRecyclerView: RecyclerView
-    private val listDataManager = ListDataManager(this)
+    private var listSelectionFragment: ListSelectionFragment = ListSelectionFragment.newInstance()
+    private var fragmentContainer: FrameLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +32,12 @@ class ListActivity : AppCompatActivity(), ListSelectionRecyclerViewAdapter.ListS
     }
 
     private fun setupViews() {
-        val lists = listDataManager.readList()
+        fragmentContainer = findViewById(R.id.fragment_container)
 
-        listsRecyclerView = findViewById(R.id.list_recyclerview)
-        listsRecyclerView.layoutManager = LinearLayoutManager(this)
-        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter(lists, this)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, listSelectionFragment)
+            .commit()
 
         fab.setOnClickListener { view ->
             showCreateListDialog()
@@ -55,11 +57,7 @@ class ListActivity : AppCompatActivity(), ListSelectionRecyclerViewAdapter.ListS
 
         builder.setPositiveButton(positiveButtonTitle) { dialog, i ->
             val list = TaskList(listTitleEditText.text.toString())
-            listDataManager.saveList(list)
-
-            val recyclerAdapter = listsRecyclerView.adapter
-                    as ListSelectionRecyclerViewAdapter
-            recyclerAdapter.addList(list)
+            listSelectionFragment.addList(list)
             dialog.dismiss()
             showListDetail(list)
         }
@@ -72,7 +70,7 @@ class ListActivity : AppCompatActivity(), ListSelectionRecyclerViewAdapter.ListS
         startActivityForResult(listDetailIntent, LIST_DETAIL_REQUEST_CODE)
     }
 
-    override fun listItemClicked(list: TaskList) {
+    override fun onListItemClicked(list: TaskList) {
         showListDetail(list)
     }
 
@@ -81,28 +79,17 @@ class ListActivity : AppCompatActivity(), ListSelectionRecyclerViewAdapter.ListS
 
         if (requestCode == LIST_DETAIL_REQUEST_CODE) {
             data?.let {
-                listDataManager.saveList(data.getParcelableExtra(INTENT_LIST_KEY))
-                updateLists()
+                listSelectionFragment.saveList(data.getParcelableExtra<TaskList>(INTENT_LIST_KEY))
             }
         }
     }
 
-    private fun updateLists() {
-        val lists = listDataManager.readList()
-        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter(lists, this)
-    }
-
-    // Options
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_list, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/com/stanledelacruz/listmaker/ListSelectionFragment.kt
+++ app/src/main/java/com/stanledelacruz/listmaker/ListSelectionFragment.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,17 +25,46 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  *
  */
-class ListSelectionFragment : Fragment() {
+class ListSelectionFragment : Fragment(), ListSelectionRecyclerViewAdapter.ListSelectionRecyclerViewClickListener {
 
-    private var listener: OnFragmentInteractionListener? = null
+    lateinit var listsRecyclerView: RecyclerView
+    lateinit var listDataManager: ListDataManager
+
+    // 1
+    private var listener: OnListItemFragmentInteractionListener? = null
     interface  OnListItemFragmentInteractionListener {
         fun onListItemClicked(list: TaskList)
     }
 
+    // 2
+    companion object {
+        fun newInstance(): ListSelectionFragment {
+            return  ListSelectionFragment()
+        }
+    }
+
+    // 3
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnListItemFragmentInteractionListener) {
+            listener = context
+            listDataManager = ListDataManager(context)
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    // 4
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupDataManager()
+    }
+
+    // 5
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,33 +72,40 @@ class ListSelectionFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list_selection, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
+    // 6
     override fun onDetach() {
         super.onDetach()
         listener = null
     }
 
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
+    private fun setupDataManager() {
+        val lists = listDataManager.readList()
+
+        view?.let {
+            listsRecyclerView = it.findViewById(R.id.list_recyclerview)
+            listsRecyclerView.layoutManager = LinearLayoutManager(activity)
+            listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter(lists, this)
+        }
     }
 
-    companion object {
-        fun newInstance(): ListSelectionFragment {
-            return  ListSelectionFragment()
-        }
+    fun addList(list: TaskList) {
+        listDataManager.saveList(list)
+
+        val recyclerViewAdapter = listsRecyclerView.adapter as ListSelectionRecyclerViewAdapter
+        recyclerViewAdapter.addList(list)
+    }
+
+    fun saveList(list: TaskList) {
+        listDataManager.saveList(list)
+        updateLists()
+    }
+
+    private fun updateLists() {
+        val lists = listDataManager.readList()
+        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter(lists, this)
+    }
+
+    override fun listItemClicked(list: TaskList) {
+        listener?.onListItemClicked(list)
     }
 }
